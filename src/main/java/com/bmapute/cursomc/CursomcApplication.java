4ab43cf41beac098f595b683feefa89aea1441d0
bmapute <beltonmapute@gmail.com>
2018-01-06 11:59:52 +0000

    Produto e assoaciacao mm com categoria

--- src/main/java/com/bmapute/cursomc/CursomcApplication.java
+++ src/main/java/com/bmapute/cursomc/CursomcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.bmapute.cursomc.domain.Categoria;
+import com.bmapute.cursomc.domain.Produto;
 import com.bmapute.cursomc.repositories.CategoriaRepository;
+import com.bmapute.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -16,6 +18,8 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	@Autowired
+	ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -23,10 +27,23 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
+		
 		Categoria cat1=new Categoria(null, "Informatica");
 		Categoria cat2=new Categoria(null, "Escritorio");
 		
+		Produto p1=new Produto(null, "Computador", 2000.00);
+		Produto p2=new Produto(null, "Impressora", 8000.00);
+		Produto p3=new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 	categoriaRepository.save(Arrays.asList(cat1,cat2));
+	produtoRepository.save(Arrays.asList(p1,p2,p3));
 		
 	}
 }
