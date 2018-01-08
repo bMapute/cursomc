e786ad5ce7da47063a11bbbc82f1c531f5853e87
bmapute <beltonmapute@gmail.com>
2018-01-08 18:27:35 +0000

    Cliente resource e service prontos

---
+++ src/main/java/com/bmapute/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,55 @@
+package com.bmapute.cursomc.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.bmapute.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+private Integer id;
+
+@NotEmpty(message="Campo obrigatorio")
+@Length(min=5, max=120, message="O tamnho deve ter entre 5 a 120 caracters ")
+private String nome;
+
+@NotEmpty(message="Campo obrigatorio")
+@Email(message="Email invalido")
+private String email;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public ClienteDTO() {
+	}
+	
+public	ClienteDTO(Cliente obj) {
+	this.id=obj.getId();
+	this.nome=obj.getNome();
+	this.email=obj.getEmail();
+}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
+	
--- src/main/java/com/bmapute/cursomc/services/ClienteService.java
+++ src/main/java/com/bmapute/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.bmapute.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.bmapute.cursomc.domain.Cliente;
+import com.bmapute.cursomc.dto.ClienteDTO;
 import com.bmapute.cursomc.repositories.ClienteRepository;
+import com.bmapute.cursomc.services.exceptions.DataIntegrityException;
 import com.bmapute.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -23,5 +31,44 @@ public class ClienteService {
 		throw new ObjectNotFoundException("Objecto nao encontrado! Id:"+id
 				+", Tipo: "+Cliente.class.getName());
 		
+	}
+	
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+		return repository.save(cliente);
+	}
+
+	public Cliente update(Cliente cliente) {
+		Cliente oldObj=find(cliente.getId());
+		updateData(oldObj,cliente);
+		return repository.save(oldObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao e possivel excluir o Cliente pois existem dados associados!");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDto(ClienteDTO clienteDto) {
+		return new Cliente(clienteDto.getId(), clienteDto.getNome(), clienteDto.getEmail(), null, null);
+	}
+	
+
+	private void updateData(Cliente oldObj, Cliente cliente) {
+		oldObj.setNome(cliente.getEmail());
+		oldObj.setEmail(cliente.getEmail());
 	}
 }
